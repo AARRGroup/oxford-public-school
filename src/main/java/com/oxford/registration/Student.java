57395b9229fdc201159669f5ad5688de61e705ab
unknown <akumarjavatech@gmail.com>
2018-07-27 23:11:59 +0530

    add student

---
+++ src/main/java/com/oxford/registration/Student.java
@@ -0,0 +1,23 @@
+package com.oxford.registration;
+
+public class Student {
+	private String studentName;
+	private String registrationNumber;
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+}
